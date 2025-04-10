fefbb7fd454514d2dfc324ccdcbd5d8bf0c4f87c
kamikaDzeUty <2ivan03112004@mail.ru>
2025-04-10 17:12:00 +0300

    Сделал реализацию методов, хранил всё в списке

--- simple_backend/src/task_tracker/main.py
+++ simple_backend/src/task_tracker/main.py
@@ -1,21 +1,40 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import List, Optional
 
 app = FastAPI()
 
-@app.get("/tasks")
+class Task(BaseModel):
+    id: int
+    name: str
+    status: str
+
+
+tasks: List[Task] = []
+
+@app.get("/tasks", response_model=List[Task])
 def get_tasks():
-    pass
+    return tasks
 
-@app.post("/tasks")
-def create_task(task):
-    pass
+@app.post("/tasks", response_model=Task)
+def create_task(task: Task):
+    if any(t.id == task.id for t in tasks):
+        raise HTTPException(status_code=400, detail='Задача с таким ID уже существует')
+    tasks.append(task)
+    return f'Задача {task} была добавлена'
 
-@app.put("/tasks/{task_id}")
-def update_task(task_id: int):
-    pass
-
+@app.put("/tasks/{task_id}", response_model=Task)
+def update_task(task_id: int, new_task:Task):
+    for index, task in enumerate(tasks):
+        if task.id == task_id:
+            tasks[index] = new_task
+            return f'Задача обновилась на {new_task}'
+    raise HTTPException(status_code=404, detail='Такой задачи нет')
 @app.delete("/tasks/{task_id}")
 def delete_task(task_id: int):
-    pass
-
+    for index, task in enumerate(tasks):
+        if task.id == task_id:
+            del tasks[index]
+            return 'Задача удалена'
+    raise HTTPException(status_code=404, detail='Такой задачи нет')
 # Сервер запустился, всё файн 👍💕
